54e8ec30f900613e7b6b580af222d1d62f05d9af
Rovshen Tagangylyjov <rovshentagangylyjov@gmail.com>
2024-12-12 17:04:47 +0500

    update

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 
 from .managers import UserManager
+from .models import User
 
 
+@admin.register(User)
 class User(AbstractUser):
     objects = UserManager()
